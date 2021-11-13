421f5a102acb84233d3f16f120671bbc1e8bee4a
Thomas Kramer <code@tkramer.ch>
2021-11-14 00:17:22 +0100

    CI: use run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run all integration tests of librecell.
+# TODO: also run unit tests with `nosetest`.
+
+set -e
+
+LCDIR=$PWD
+
+echo TEST: lclayout
+cd $LCDIR/librecell-layout/tests
+./run_dummy_tech_tests.sh
+
+echo TEST: lctime
+cd $LCDIR/librecell-lib/tests
+./run_freepdk45_test.sh
+
+
+echo TEST OK
