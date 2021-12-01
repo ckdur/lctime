20f3561eb51aa1eec35429d8ada71f90627fcfe4
Thomas Kramer <code@tkramer.ch>
2021-12-01 11:10:21 +0100

    Bump version.

--- librecell-meta/setup.py
+++ librecell-meta/setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='librecell',
-      version='0.0.13',
+      version='0.0.15',
       description='Meta-package for the LibreCell suite.',
       long_description=readme(),
       long_description_content_type="text/markdown",
@@ -23,8 +23,8 @@ setup(name='librecell',
       author_email='code@tkramer.ch',
       license='',  # ???
       install_requires=[
-          'librecell-common==0.0.13',
-          'librecell-layout==0.0.13',
-          'librecell-lib==0.0.13',
+          'librecell-common==0.0.15',
+          'librecell-layout==0.0.15',
+          'librecell-lib==0.0.15',
       ],
       zip_safe=False)
